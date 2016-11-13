3dcd041b3c67635cce5645a63ed276fcd058f85c
Miron Jakubowski <mijakubowski@gmail.com>
2016-11-13 13:17:38 +0100

    repeat password if there is new records file

--- main.py
+++ main.py
@@ -13,12 +13,20 @@ parser.add_argument("db", help="Path to database file. If file doesn't exists it
 args = parser.parse_args()
 
 cnt = 0
-while not Storage.load(args.db, getpass()):
+pw = getpass()
+while not Storage.load(args.db, pw):
     cnt = cnt + 1
     if cnt >= 3:
         print("Access Denied")
         exit()
+    pw = getpass()
 
+# repeat password for new db file
+if Storage.is_new:
+    rep_pass = getpass(prompt="Repeat Password: ")
+    if rep_pass != pw:
+        print("Password do to match. ")
+        exit()
 
 views = {
     "MainView": MainView(),
